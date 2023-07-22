2e7ea6cf0ab569be0d11170b1f0296fb27b4c0a0
the Black Cat <git@theblack.cat>
2023-07-22 18:30:41 +0200

    v2.0 Revamped Webpage

--- resources/scripts/totop.js
+++ resources/scripts/totop.js
@@ -1,19 +1,14 @@
-//Get the button
 var mybutton = document.getElementById("goTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+window.addEventListener('scroll', function() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
-}
+});
 
-// When the user clicks on the button, scroll to the top of the document
 function goToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.style.scrollBehavior = 'smooth';
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
