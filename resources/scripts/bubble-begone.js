9d6c24de390915b2d9fb81dc5704dec498fc8d5b
the Black Cat <git@theblack.cat>
2023-07-22 19:02:47 +0200

    More fixes

--- resources/scripts/bubble-begone.js
+++ resources/scripts/bubble-begone.js
@@ -1,13 +1,23 @@
 const bubbleTrigger = document.querySelector('.bubble-trigger-bug');
 const bubble = document.querySelector('.bubble-bug');
 
+
 bubbleTrigger.addEventListener('mouseenter', () => {
   bubble.style.display = 'block';
 });
+
+bubbleTrigger.addEventListener('click', () => {
+  bubble.style.display = 'block';
+});
+
+
 bubbleTrigger.addEventListener('mouseleave', () => {
   bubble.style.display = 'none';
 });
 
-bubble.addEventListener('mouseleave', () => {
-  bubble.style.display = 'none';
+
+document.addEventListener('click', (event) => {
+  if (!bubble.contains(event.target) && !bubbleTrigger.contains(event.target)) {
+    bubble.style.display = 'none';
+  }
 });
